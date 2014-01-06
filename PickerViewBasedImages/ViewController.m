546c129bf52156c8b38e2a689cb1b773c4798ae6
SDT1 <eyebeam79@naver.com>
2014-01-06 15:49:51 +0900

    3.3.8. 실습을 완료했습니다.

--- PickerViewBasedImages/ViewController.m
+++ PickerViewBasedImages/ViewController.m
@@ -8,11 +8,56 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define MAX_NUM 10
+
+@interface ViewController ()<UIPickerViewDataSource, UIPickerViewDelegate>
+@property (weak, nonatomic) IBOutlet UIPickerView *picker;
 
 @end
 
 @implementation ViewController
+
+- (IBAction)selectRandom:(id)sender
+{
+    int r = arc4random() % MAX_NUM;
+    [self.picker selectRow:r inComponent:0 animated:YES];
+}
+
+// 컴포넌트개수
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+- (NSInteger) pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return MAX_NUM;
+}
+
+- (CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component
+{
+    return 64;
+}
+
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view
+{
+    NSString *imagePath = [NSString stringWithFormat:@"flag%d.png", row];
+    UIImage *image = [UIImage imageNamed:imagePath];
+    UIImageView *imageView;
+    
+    if (view == nil)
+    {
+        imageView = [[UIImageView alloc] initWithImage:image];
+        imageView.frame = CGRectMake(0, 0, 100, 60);
+    }
+    else
+    {
+        imageView = (UIImageView *)view;
+        imageView.image = image;
+    }
+    
+    return imageView;
+}
 
 - (void)viewDidLoad
 {
